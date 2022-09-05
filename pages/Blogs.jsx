d32e34e7d8d6fe11daef604d70993071757d6695
JoshMuasya <muasyajoshua07@gmail.com>
2022-09-05 11:56:09 +0300

    Made changes in Blog Pages

--- pages/Blogs.jsx
+++ pages/Blogs.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Blogs = () => {
   return (
-    <div>Blogs</div>
+    <div className='h-screen flex flex-col justify-center align-center items-center'>
+      <div>
+        <h3>
+          Our trips in just some few lines 
+        </h3>
+      </div>
+
+      <div>
+        <h4>
+          Our Blogs
+        </h4>
+      </div>
+
+
+    </div>
   )
 }
 
